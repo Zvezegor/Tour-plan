147648be7d6112cbbf1953fb74bb85a44b2113cf
Zvezegor <ferocaritas@gmail.com>
2022-05-03 18:32:07 +0300

    s

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-$('.newsletter').parallax({imageSrc: '../img/newsletter-bg.jpeg'});
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+$(".newsletter").parallax({ imageSrc: "../img/newsletter-bg.jpeg" });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
